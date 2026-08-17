9ff19989ee4b751ea0f2a37b96fbff8b9aab0d60
Masengesho Bruno <masengeshobruno4@gmail.com>
2026-08-17 11:27:10 +0200

    clean: remove temporary scripts

--- apps/api/src/scripts/find-schema-telemetry.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const schema = fs.readFileSync('F:/emoto-fleet-os/emoto-fleet-os-backend/prisma/schema.prisma', 'utf8');
-const tpIdx = schema.indexOf('model TelemetryPoint');
-if (tpIdx !== -1) {
-  console.log(schema.slice(tpIdx, tpIdx + 800));
-} else {
-  console.log('TelemetryPoint model not found');
-}
